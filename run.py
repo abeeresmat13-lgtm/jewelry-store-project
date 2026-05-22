33e908a10fdec5963a68b968e43837a65d1baabf
Sandra Samy <sandrasamydev@gmail.com>
2026-05-22 21:58:39 +0300

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# استدعاء الـ Factory وبناء التطبيق
-app = create_app()
-
-if __name__ == '__main__':
-    # تشغيل السيرفر بنفس الإعدادات بتاعتك
-    app.run(host='0.0.0.0', port=5000, debug=False)
